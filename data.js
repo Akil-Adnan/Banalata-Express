81008fc0eed1a8e8efa21d9f6a2aa29d8b0d0986
Akil Adnan <akiladnan.abir04@gmail.com>
2026-01-01 20:21:05 +0600

    V2 of the website
    Major overhaul since the last commit

---
+++ data.js
@@ -0,0 +1,105 @@
+// Nav Links
+export const siteLinks = [
+  { text: "Home", href: "#home" },
+  { text: "About", href: "#about" },
+  { text: "Cast", href: "#cast" },
+  { text: "Gallery", href: "#gallery" },
+  { text: "Contact", href: "#contact" }
+];
+
+// Social Links
+export const socialLinksData = [
+  {
+    href: "https://www.facebook.com/p/Buriganga-Talkies-61582315140804/",
+    icon: "fa-facebook"
+  },
+  {
+    href: "",
+    icon: "fa-instagram"
+  },
+  {
+    href: "",
+    icon: "fa-youtube"
+  }
+];
+
+// Cast data
+export const castData = [
+  {
+    name: "Mosharraf Karim",
+    img: "https://outspoken.newagebd.com/files/img/202409/c85d710f9345886c6a57c1bb42fc52d6.jpg"
+  },
+  {
+    name: "Chanchal Chowdhury",
+    img: "https://assets.telegraphindia.com/telegraph/2022/Dec/1670612919_chanchal.jpg"
+  },
+  {
+    name: "Azmeri Haque Badhon",
+    img: "https://www.desiblitz.com/wp-content/uploads/2025/07/Azmeri-Haque-Badhon-says-Industry-Friends-turned-Against-Her-f.jpg"
+  },
+  {
+    name: "Sabila Nur",
+    img: "https://www.thedailystar.net/sites/default/files/styles/big_201/public/images/2023/04/01/smm_6605.jpg"
+  },
+  {
+    name: "Shamol Mawla",
+    img: "https://d2u0ktu8omkpf6.cloudfront.net/e4ab8fa4e84dd538b39b486f3987a5bf500df8b6d5995e28.jpg"
+  }
+];
+
+
+// Carousel Image Data
+export const carouselSlides = [
+  {
+    src: "https://scontent.fdac151-1.fna.fbcdn.net/v/t39.30808-6/592862549_122110004535077171_6663321955648925262_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=fp-4-gcMegsQ7kNvwFzat4D&_nc_oc=AdnB25QVTr27CTXQZE1_rR_6zbk6JX2iTPAn-HFdgzpFCAhnzaRA2p3i0pHj_xk_kPA&_nc_zt=23&_nc_ht=scontent.fdac151-1.fna&_nc_gid=GyZS_R3m4wY0PC0vtpHKFw&oh=00_AfqpjFcnsWVlS029k2sMyNakN_a2MLHEsYtW6fiHVhJEWA&oe=695BFBC5",
+    alt: "Mountain Landscape"
+  },
+  {
+    src: "https://scontent.fdac151-1.fna.fbcdn.net/v/t39.30808-6/593765751_122110004391077171_7137512250546616689_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=u21s268edpQQ7kNvwFcTVlq&_nc_oc=Adm-lNv4giVdoQ_DonlInAV450osquxwvMrYf8GHfo1w1iT6vKvXNit_XSNU5AMluZE&_nc_zt=23&_nc_ht=scontent.fdac151-1.fna&_nc_gid=4PuDGtgRyQ3E2VlHiHb96Q",
+    alt: "Beach Sunset"
+  },
+  {
+    src: "https://scontent.fdac151-1.fna.fbcdn.net/v/t39.30808-6/592059097_122110004463077171_4870142053065016972_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=CCOEPUpbLMIQ7kNvwGMlpFI&_nc_oc=AdnQALxCzHB1sIh_gtm-Bo8vGDpXOJjxWU8If3GkF6s8XKETRHh7LFN4X7_1mKpU7HU&_nc_zt=23&_nc_ht=scontent.fdac151-1.fna&_nc_gid=Ysj88LRZ1Omf_B0iYcpA9Q",
+    alt: "Beach Sunset"
+  },
+  {
+    src: "https://scontent.fdac151-1.fna.fbcdn.net/v/t39.30808-6/581934318_122106680505077171_8102046010505362098_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=uK_DXFE5KPcQ7kNvwFoY2sJ&_nc_oc=AdlmcST9dhXPfRvjy2fotNTB5NNe075Fa8CFbi8wLxWXshOgBnlv8a2rSxfc_maLo04&_nc_zt=23&_nc_ht=scontent.fdac151-1.fna&_nc_gid=hD79Zygp0Y3iRZXrj1BOdA",
+    alt: "Beach Sunset"
+  },
+  {
+    src: "assets/cover.jpg",
+    alt: "Cover Image"
+  }
+];
+
+// Footer Contact Info
+export const contactInfo = [
+  {
+    icon: "fa-location-dot",
+    text: "Block A, 2nd Floor, House 53, Road 01,<br />Niketan Housing Society, Dhaka, Bangladesh"
+  },
+  {
+    icon: "fa-phone",
+    text: "+880 1756 407517"
+  },
+  {
+    icon: "fa-envelope",
+    text: "contact.dopeproductions@gmail.com"
+  }
+];
+
+
+// YouTube Link
+export const youTubeLink = "https://www.youtube.com/embed/PE1k3TdwK7Y";
+
+
+export const backgroundImages = [
+    {
+        id: "about",
+        url: "../assets/about-background.png"
+    },
+    {
+        id: "cast",
+        url: '../assets/cast-background.jpg'
+    }
+]
--- script.js
+++ script.js
@@ -1,30 +1,209 @@
+import { siteLinks, socialLinksData, castData, carouselSlides, contactInfo, youTubeLink, backgroundImages } from "./data.js";
+
+/* ---------- Nav & Aside Link Function ---------- */
+function renderLinks({ container, links, liClass = "", aClass = "", addActiveToFirst = false }) {
+  if (!container) return;
+
+  container.innerHTML = links
+    .map(({ text, href }, index) => {
+      const liCls = liClass ? ` class="${liClass}"` : "";
+      const aCls = aClass ? ` class="${aClass}${addActiveToFirst && index === 0 ? " active" : ""}"` : "";
+      return `
+        <li${liCls}>
+          <a${aCls} href="${href}">${text}</a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+
+/* ---------- Nav ---------- */
+renderLinks({
+  container: document.getElementById("navLinks"),
+  links: siteLinks,
+  aClass: "nav__link",
+  addActiveToFirst: false
+});
+
+/* ---------- Aside ---------- */
+renderLinks({
+  container: document.getElementById("asideLinks"),
+  links: siteLinks,
+  liClass: "aside__link"
+});
+
+
+
+/* ---------- Social Media Handles ---------- */
+function renderSocialLinks(container, links) {
+  container.innerHTML = links
+    .map(
+      ({ href, icon }) => `
+        <a href="${href}" target="_blank" rel="noopener">
+          <i class="fa-brands ${icon}"></i>
+        </a>`
+    )
+    .join("");
+}
+
+renderSocialLinks(
+  document.getElementById("asideSocialLinks"),
+  socialLinksData
+);
+
+renderSocialLinks(
+  document.getElementById("homeSocialLinks"),
+  socialLinksData
+);
+
+
+/* ---------- Cast Information ---------- */
+const castGrid = document.getElementById("castGrid");
+
+castGrid.innerHTML = castData
+  .map(
+    (cast, index) => `
+      <div class="cast__card" style="--index: ${index}">
+        <div class="cast__card-items">
+          <h3>${cast.name}</h3>
+          <img src="${cast.img}" alt="${cast.name}" />
+        </div>
+      </div>
+    `
+  )
+  .join("");
+
+
+/* ---------- Cast Information ---------- */
+const indicatorsEl = document.getElementById("carouselIndicators");
+const innerEl = document.getElementById("carouselInner");
+
+// Render indicators
+indicatorsEl.innerHTML = carouselSlides
+  .map((_, i) => `
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${i}" 
+      ${i === 0 ? 'class="active" aria-current="true"' : ''} 
+      aria-label="Slide ${i + 1}">
+    </button>
+  `)
+  .join("");
+
+// Render slides
+carouselSlides.forEach((slide, i) => {
+  const div = document.createElement("div");
+  div.classList.add("carousel-item");
+  if (i === 0) div.classList.add("active");
+
+  const img = document.createElement("img");
+  img.src = slide.src;
+  img.alt = slide.alt;
+  img.classList.add("d-block", "w-100");
+
+  div.appendChild(img);
+  innerEl.appendChild(div);
+});
+
+/* ---------- Contact Information ---------- */
+const contactEl = document.getElementById("contactInfo");
+
+contactInfo.forEach(item => {
+  const div = document.createElement("div");
+  div.classList.add("footer-item");
+
+  div.innerHTML = `
+    <i class="fa-solid ${item.icon}"></i>
+    <p>${item.text}</p>
+  `;
+
+  contactEl.appendChild(div);
+});
+
+
+/* ---------- Navigation Bar ---------- */
+const sections = document.querySelectorAll("section");
+const navLinksContainer = document.getElementById("navLinks");
+const navLinks = navLinksContainer.querySelectorAll(".nav__link");
+
+function updateActiveNav() {
+  const scrollPos = window.scrollY + window.innerHeight / 2;
+  let currentSection = null;
+
+  sections.forEach(section => {
+    const top = section.offsetTop;
+    const bottom = top + section.offsetHeight;
+    if (scrollPos >= top && scrollPos < bottom) {
+      currentSection = section;
+    }
+  });
+
+  if (currentSection) {
+    const id = currentSection.getAttribute("id");
+
+    // Remove active only from nav links in this container
+    navLinks.forEach(link => link.classList.remove("active"));
+
+    const navLink = navLinksContainer.querySelector(`.nav__link[href="#${id}"]`);
+    if (navLink) navLink.classList.add("active");
+  }
+}
+
+// Scroll listener with requestAnimationFrame
+let ticking = false;
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      updateActiveNav();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
+
+// Initial check
+updateActiveNav();
+
+
+
+/* ---------- Sidebar Open And Close Functionality ---------- */
 const sidebar = document.querySelector('.sidebar');
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
-const aside_links = document.querySelectorAll('.aside__link')
+const asideLinks = document.querySelectorAll('.aside__link')
+const asideOverlay = document.querySelector('.aside__overlay')
 
 hamburger.addEventListener('click', () => {
   sidebar.classList.remove('hide');
+  asideOverlay.classList.remove('hidden');
 });
 
 // Close sidebar
 close.addEventListener('click', () => {
   sidebar.classList.add('hide');
+  asideOverlay.classList.add('hidden');
 });
 
-aside_links.forEach(link => {
+
+asideOverlay.addEventListener('click', () => {
+  sidebar.classList.add('hide');
+  asideOverlay.classList.add('hidden');
+});
+
+asideLinks.forEach(link => {
   link.addEventListener('click', () => {
     sidebar.classList.add('hide');
+    asideOverlay.classList.add('hidden');
   });
 });
 
 
+/* ---------- Modal Video Functionality ---------- */
 const modal = document.getElementById('videoModal');
-const openBtn = document.querySelector('.about__button');
+const openBtn = document.querySelector('.display__button');
 const closeBtn = document.getElementById('closeModal');
 const player = document.getElementById('ytPlayer');
 
-const YT_URL = "https://www.youtube.com/embed/PE1k3TdwK7Y";
+const YT_URL = youTubeLink;
 
 openBtn.addEventListener('click', () => {
   modal.classList.remove('hidden'); // Show modal
@@ -46,28 +225,57 @@ document.addEventListener('keydown', (e) => {
 });
 
 
-// Select all sections and nav links
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav__link");
+/* ---------- Modify cast card position property feature ---------- */
+const cards = document.querySelectorAll('.cast__card');
 
-// Intersection Observer options
-const options = {
-  root: null,       // viewport
-  threshold: 0.5    // 50% of section visible
-};
+cards.forEach((card, index) => {
+  card.style.setProperty('--index', index);
+});
 
-// Callback for Intersection Observer
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    const id = entry.target.getAttribute("id");
-    const navLink = document.querySelector(`.nav__link[href="#${id}"]`);
 
-    if (entry.isIntersecting) {
-      navLinks.forEach(link => link.classList.remove("active"));
-      navLink.classList.add("active");                           
-    }
-  });
-}, options);
 
-// Observe each section
-sections.forEach(section => observer.observe(section));
+/* ---------- Video on and off feature ---------- */
+const videoSections = document.querySelectorAll('section');
+let currentlyPlayingVideo = null;
+
+const sectionVideoObserver = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      const backgroundVideo = entry.target.querySelector('.bg-video');
+      if (!backgroundVideo) return;
+
+      if (entry.isIntersecting) {
+        // Pause any previously playing video
+        if (
+          currentlyPlayingVideo &&
+          currentlyPlayingVideo !== backgroundVideo
+        ) {
+          currentlyPlayingVideo.pause();
+        }
+
+        entry.target.classList.add('is-active');
+        currentlyPlayingVideo = backgroundVideo;
+        backgroundVideo.play();
+      } else {
+        entry.target.classList.remove('is-active');
+        backgroundVideo.pause();
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+videoSections.forEach(section =>
+  sectionVideoObserver.observe(section)
+);
+
+
+/* ---------- Background Images ---------- */
+backgroundImages.forEach(bg => {
+  const section = document.getElementById(bg.id);
+  if (section) {
+    section.style.backgroundImage = `url('${bg.url}')`;
+  }
+});
